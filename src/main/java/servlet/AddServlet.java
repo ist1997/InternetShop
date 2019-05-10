ee5b6de2b76171e631514de02e9c4e2dff17c0f7
ist1997 <ilkivsviatoslav@gmail.com>
2019-05-10 14:38:20 +0300

    Homework 18 (suggested changes)

--- src/main/java/servlet/AddServlet.java
+++ src/main/java/servlet/AddServlet.java
@@ -8,12 +8,13 @@ import model.User;
 import org.apache.log4j.Logger;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@javax.servlet.annotation.WebServlet(name = "AddServlet", value = "/add")
+@WebServlet(name = "AddServlet", value = "/add")
 
 public class AddServlet extends HttpServlet {
 
---
+++ web/access_denied.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: ilkiv
+  Date: 10.05.2019
+  Time: 14:12
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Access denied</title>
+</head>
+<body>
+Access denied!
+</body>
+</html>
